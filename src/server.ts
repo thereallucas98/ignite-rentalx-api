ca12eeec80c7f635af93006eca5bedb9934dddfb
thereallucas98 <lucas1998david@gmail.com>
2021-12-25 15:31:28 -0300

    Instalando o typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,17 @@
-import express from "express";
-import swaggerUi from "swagger-ui-express";
+import express from 'express';
+import swaggerUi from 'swagger-ui-express';
 
-import swaggerFile from "./swagger.json";
-import { router } from "./routes";
+import swaggerFile from './swagger.json';
+import { router } from './routes';
+
+import './database';
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 app.use(router);
 
-app.listen(3333, () => console.log("Server is running on port 3333! 🥳"))
+app.listen(3333, () => console.log('Server is running on port 3333! 🥳'));
