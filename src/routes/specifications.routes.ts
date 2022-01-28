e3ab770f2c67fd54a50cc8b3539f16386d8ec706
thereallucas98 <lucas1998david@gmail.com>
2022-01-27 21:26:19 -0300

    Autenticaçao nas rotas

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,4 +1,5 @@
-import { Request, Response, Router } from "express";
+import { Router } from "express";
+import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 import { CreateSpecificationController } from "../modules/cars/useCases/createSpecification/CreateSpecificationController";
 import { ListSpecificationsController } from "../modules/cars/useCases/listSpecifications/ListSpecificationsController";
 
@@ -7,6 +8,8 @@ const specificationsRouter = Router();
 
 const createSpecificationController = new CreateSpecificationController();
 const listSpecificationController = new ListSpecificationsController();
+
+specificationsRouter.use(ensureAuthenticated);
 
 specificationsRouter.post("/", createSpecificationController.handle);
 
