c690b30f0f23f6dde760daabc99247c84359213b
thereallucas98 <lucas1998david@gmail.com>
2022-02-09 20:17:17 -0300

    Upload de avatar

--- src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
+++ src/modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarUseCase.ts
@@ -1,6 +1,8 @@
 import { inject, injectable } from "tsyringe";
 import { UsersRepository } from "../../repositories/implementations/UsersRepository";
 
+import { deleteFile } from "../../../../utils/file";
+
 interface IRequest {
   user_id: string;
   avatar_file: string;
@@ -15,6 +17,10 @@ class UpdateUserAvatarUserCase {
 
   async execute({ user_id, avatar_file }: IRequest): Promise<void> {
     const user = await this.usersRepository.findById(user_id);
+
+    if (user.avatar) {
+      await deleteFile(`./tmp/avatar/${user.avatar}`);
+    }
 
     user.avatar = avatar_file;
 
