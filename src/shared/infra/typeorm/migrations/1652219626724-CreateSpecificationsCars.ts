e1488b3290c73660ef6c89f72826ef021bb07954
thereallucas98 <lucas1998david@gmail.com>
2022-05-10 19:01:11 -0300

    Criando migrations de especificação de carros

---
+++ src/shared/infra/typeorm/migrations/1652219626724-CreateSpecificationsCars.ts
@@ -0,0 +1,72 @@
+import {
+  MigrationInterface,
+  QueryRunner,
+  Table,
+  TableForeignKey,
+} from "typeorm";
+
+export class CreateSpecificationsCars1652219626724
+  implements MigrationInterface
+{
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "specifications_cars",
+        columns: [
+          {
+            name: "car_id",
+            type: "uuid",
+          },
+          {
+            name: "specification_id",
+            type: "uuid",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "now()",
+          },
+        ],
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      "specifications_cars",
+      new TableForeignKey({
+        name: "FKSpecificationCar",
+        referencedTableName: "specifications",
+        referencedColumnNames: ["id"],
+        columnNames: ["specification_id"],
+        // Quando a tabela pai sofrer alguma alteração
+        onDelete: "SET NULL",
+        onUpdate: "SET NULL",
+      })
+    );
+
+    await queryRunner.createForeignKey(
+      "specifications_cars",
+      new TableForeignKey({
+        name: "FKCarSpecification",
+        referencedTableName: "cars",
+        referencedColumnNames: ["id"],
+        columnNames: ["car_id"],
+        // Quando a tabela pai sofrer alguma alteração
+        onDelete: "SET NULL",
+        onUpdate: "SET NULL",
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropForeignKey(
+      "specifications_car",
+      "FKCarSpecification"
+    );
+    await queryRunner.dropForeignKey(
+      "specifications_car",
+      "FKSpecificationCar"
+    );
+
+    await queryRunner.dropTable("specifications_car");
+  }
+}
