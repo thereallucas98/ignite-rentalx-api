093692a0710a18a6d8fd82651bab4a21d23ec7b1
thereallucas98 <lucas1998david@gmail.com>
2022-07-02 08:47:17 -0300

    Controller de devolução de carro

--- src/modules/rentals/infra/repositories/RentalsRepository.ts
+++ src/modules/rentals/infra/repositories/RentalsRepository.ts
@@ -12,20 +12,6 @@ class RentalsRepository implements IRentalsRepository {
     this.repository = getRepository(Rental);
   }
 
-  async findOpenRentalByCar(car_id: string): Promise<Rental> {
-    const openByCar = await this.repository.findOne({
-      where: { car_id, end_date: null },
-    });
-    return openByCar;
-  }
-
-  async findOpenRentalByUser(user_id: string): Promise<Rental> {
-    const openByUser = await this.repository.findOne({
-      where: { user_id, end_date: null },
-    });
-    return openByUser;
-  }
-
   async create({
     car_id,
     expected_return_date,
@@ -60,6 +46,20 @@ class RentalsRepository implements IRentalsRepository {
     });
 
     return rentals;
+  }
+
+  async findOpenRentalByCar(car_id: string): Promise<Rental> {
+    const openByCar = await this.repository.findOne({
+      where: { car_id, end_date: null },
+    });
+    return openByCar;
+  }
+
+  async findOpenRentalByUser(user_id: string): Promise<Rental> {
+    const openByUser = await this.repository.findOne({
+      where: { user_id, end_date: null },
+    });
+    return openByUser;
   }
 }
 
